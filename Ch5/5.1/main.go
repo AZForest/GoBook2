4b502dbf0827524ce8a3331dd567f8ffcccebb2e
Alex Forest <azforest44@gmail.com>
2025-08-03 12:05:09 -0700

    Aug3b

--- Ch5/5.1/main.go
+++ Ch5/5.1/main.go
@@ -19,23 +19,27 @@ func main() {
 		os.Exit(1)
 	}
 	//5.1
-	counter := 0
-	for _, link := range visit(nil, doc) {
-		counter++
-		fmt.Println(link)
-	}
-	fmt.Printf("Total Links: %d\n", counter)
+	// counter := 0
+	// for _, link := range visit(nil, doc) {
+	// 	counter++
+	// 	fmt.Println(link)
+	// }
+	// fmt.Printf("Total Links: %d\n", counter)
 
 	//5.4 main 
-	m := visiter(nil, doc)
+	newMap := map[string][]string{
+		"links": {""},
+		"script": {""},
+		"image": {""},
+		"style": {""},
+	}
+	m := visiter(newMap, doc)
 	for k, v := range m {
 		fmt.Printf("Key: %s\tVal: %v\n", k, v)
-		// for _, val := range v {
-		// 	fmt.Printf("%s\n", val)
-		// }
 	}
 }
 
+// 5.1
 // visit appends to links each link found in n and returns the result.
 func visit(links []string, n *html.Node) []string {
 	if n.Type == html.ElementNode && n.Data == "a" {
@@ -57,26 +61,42 @@ func visit(links []string, n *html.Node) []string {
 	return links
 }
 
+// 5.4
 // Extend the visit function s o that it extracts other kinds of links from the doc-
 // ument, such as images, scripts, and style sheets.
 // visit appends to links each link found in n and returns the result.
-func visiter(tracker map[string]string, n *html.Node) map[string]string {
+func visiter(tracker map[string][]string, n *html.Node) map[string][]string {
+	// fmt.Printf("%v ", n.Data)
 	if n.Type == html.ElementNode {
 		switch n.Data {
 		case "a":
 			for _, a := range n.Attr {
 				if a.Key == "href" {
-					tracker["links"] += a.Val
+					tracker["links"] = append(tracker["links"], a.Val)
 				}
 			}
 		case "script":
-			tracker["script"] += n.Data
-		case "image":
-			tracker["image"] += n.Data
-		case "style":
-			tracker["style"] += n.Data
+			for _, s := range n.Attr {
+				tracker["script"] = append(tracker["script"], s.Val)
+			}
+		case "img":
+			for _, i := range n.Attr {
+				if i.Key == "src" {
+					tracker["image"] = append(tracker["image"], i.Val)
+				}
+			}
+		case "link":
+			for _, i := range n.Attr {
+				if i.Key == "rel" && i.Val == "stylesheet" {
+					for _, a := range n.Attr {
+						if a.Key == "href" {
+							tracker["style"] = append(tracker["style"], a.Val)
+						}
+					}	
+				}
+			}
 		default:
-			fmt.Printf("No matches.\n")
+			//fmt.Printf("No matches.\n")
 		}
 		}
 	 
